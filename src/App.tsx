8062264a6b7107408126d6e6eafcb2d9d295e8d2
Ben Brady <benbrady04.bb@gmail.com>
2025-05-08 19:38:00 +0100

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,154 @@
-import { createSignal, Show, type ComponentProps } from "solid-js";
+import {
+    createEffect,
+    createMemo,
+    createSignal,
+    For,
+    Show,
+    type Accessor,
+    type ComponentProps,
+} from "solid-js";
 import sample from "lodash/sample";
 
 const roles = ["blind", "deaf", "mute", "ludite"] as const;
 type Role = (typeof roles)[number];
 const generateRandomRole = (): Role => sample(roles);
 
+type PlayerListHook = {
+    players: Accessor<string[]>;
+    addPlayer: (name: string) => void;
+    removePlayer: (name: string) => void;
+};
+
+const usePlayerList = (): PlayerListHook => {
+    const json = localStorage.getItem("players") ?? "[]";
+    const [players, setPlayers] = createSignal<string[]>(JSON.parse(json));
+
+    const save = () => {
+        localStorage.setItem("players", JSON.stringify(players()));
+    };
+
+    const addPlayer = (name: string) => {
+        if (players().includes(name)) return;
+        setPlayers([...players(), name]);
+        save();
+    };
+    const removePlayer = (name: string) => {
+        const newPlayers = players().filter((v) => v !== name);
+        setPlayers(newPlayers);
+        save();
+    };
+
+    return { players, addPlayer, removePlayer };
+};
+
+type RoleAssigned = {
+    name: string;
+    role: Role;
+};
+const useAssignedRoles = (
+    players: Accessor<string[]>,
+): { reroll: () => void; playerRoles: Accessor<RoleAssigned[]> } => {
+    let assignedRoles: Record<string, Role> = {};
+
+    const generateRoles = (players: string[]): RoleAssigned[] => {
+        return players.map((name) => {
+            if (name in assignedRoles) {
+                const role = assignedRoles[name];
+                return { name, role };
+            } else {
+                const role = generateRandomRole();
+                assignedRoles[name] = role;
+                return { name, role };
+            }
+        });
+    };
+
+    const [playerRoles, setPlayerRoles] = createSignal<RoleAssigned[]>(generateRoles(players()));
+
+    const reroll = () => {
+        assignedRoles = {};
+        setPlayerRoles(generateRoles(players()));
+    };
+
+    createEffect(() => {
+        setPlayerRoles(generateRoles(players()));
+    });
+
+    return { playerRoles, reroll };
+};
+
 function App() {
-    const [role, setRole] = createSignal<Role>(generateRandomRole());
+    const { addPlayer, players, removePlayer } = usePlayerList();
+    const { playerRoles, reroll } = useAssignedRoles(players);
 
     return (
-        <div class="size-full flex flex-col justify-center items-center gap-4 px-4">
-            <div class="bg-[#202020] rounded-lg py-3 px-5 flex flex-col gap-2">
-                <Show when={role() === "blind"}>
-                    <Title>Blind</Title>
-                    <p>Turn your gamma to 12 in graphics settings</p>
-                </Show>
-                <Show when={role() === "deaf"}>
-                    <Title>Deaf</Title>
-                    <p>Turn your ingame volume to 0% in the settings</p>
-                </Show>
-                <Show when={role() === "mute"}>
-                    <Title>Mute</Title>
-                    <p>
-                        Set your mic to push to talk and rebind the key to something you won't press
-                    </p>
-                </Show>
-                <Show when={role() === "ludite"}>
-                    <Title>Ludite</Title>
-                    <p>Type /cinematic in chat to turn off your HUD</p>
-                </Show>
+        <div class="size-full flex justify-center items-center">
+            <div class="h-fit flex gap-8 justify-around items-center py-16">
+                <div class="h-128 flex flex-col gap-4">
+                    <RoleCard
+                        title="Blind"
+                        description="Turn your gamma to 12 in graphics settings"
+                    />
+                    <RoleCard
+                        title="Deaf"
+                        description="Turn your ingame volume to 0% in the settings"
+                    />
+                    <RoleCard
+                        title="Mute"
+                        description="Set your mic to push to talk and rebind the key to something you won't press"
+                    />
+                    <RoleCard
+                        title="Ludite"
+                        description="Type /cinematic in chat to turn off your HUD"
+                    />
+                </div>
+                <div class="h-128 flex flex-col items-stretch gap-2">
+                    <div class="flex flex-col gap-2 flex-1">
+                        <For each={playerRoles()}>
+                            {({ name, role }) => (
+                                <div
+                                    class="hover:italic hover:underline w-fit capitalize text-xl cursor-pointer"
+                                    onClick={() => removePlayer(name)}
+                                >
+                                    {name}: {role}
+                                </div>
+                            )}
+                        </For>
+                    </div>
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            const input = e.currentTarget.querySelector("input")!;
+                            if (input.value === "") return;
+                            addPlayer(input.value);
+                            input.value = "";
+                        }}
+                        class="flex items-center gap-4"
+                    >
+                        <input class="bg-[#202020] h-8 rounded-md outline-none" name="name" />
+                        <button
+                            class="bg-[#202020] px-4 py-1 h-8 rounded-md cursor-pointer "
+                            type="submit"
+                        >
+                            Add Player
+                        </button>
+                    </form>
+                    <button
+                        class="bg-[#202020] px-4 py-2 h-fit rounded-md cursor-pointer w-full "
+                        onClick={() => reroll()}
+                    >
+                        Reroll
+                    </button>
+                </div>
             </div>
-            <button class="h-fit" onClick={() => setRole(generateRandomRole())}>
-                Generate New Role
-            </button>
         </div>
     );
 }
 
-const Title = (props: ComponentProps<"div">) => (
-    <h2 {...props} class="w-full text-center text-2xl font-bold" />
+const RoleCard = ({ title, description }: { title: string; description: string }) => (
+    <div class="bg-[#202020] w-96 rounded-lg py-3 px-5 flex flex-col justify-center gap-2 flex-1">
+        <h2 class="w-full text-2xl font-bold">{title}</h2>
+        <p>{description}</p>
+    </div>
 );
 export default App;
